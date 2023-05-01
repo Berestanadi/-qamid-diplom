6031d3e3f2fd0f72915b418311195c66c35efa03
Надежда Берестова <berestanadi@gmail.com>
2023-05-01 17:32:03 +0300

    Fix Automation block

--- fmh-android/app/src/androidTest/java/ru/iteco/fmhandroid/ui/test/NewsScreenTest.java
+++ fmh-android/app/src/androidTest/java/ru/iteco/fmhandroid/ui/test/NewsScreenTest.java
@@ -1,17 +1,13 @@
 package ru.iteco.fmhandroid.ui.test;
-
 import static androidx.test.espresso.action.ViewActions.click;
 
 import android.os.SystemClock;
-
 import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.rule.ActivityTestRule;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import io.qameta.allure.android.runners.AllureAndroidJUnit4;
 import io.qameta.allure.kotlin.junit4.DisplayName;
 import ru.iteco.fmhandroid.ui.AppActivity;
